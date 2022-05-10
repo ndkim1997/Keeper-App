e61dead30543ca9d337fe189d55617c2f680a902
ndkim1997 <kim.nick7@gmail.com>
2022-05-10 01:12:48 -0700

    arrow functions

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
     return (
-    <div>
-        <Header />
-        <Note />
-        <Footer />
-    </div>
+        <div>
+            <Header />
+            {notes.map((noteItem) =>
+                <Note
+                    key={noteItem.key}
+                    title={noteItem.title}
+                    content={noteItem.content}
+                />
+            )}
+            <Footer />
+        </div>
     );
 }
 
